2de3a86f7c908eaa6f54b6cafa80f6579f259fce
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-01-13 15:34:13 -0700

    Problem 2 start

---
+++ objectEOM.m
@@ -0,0 +1,23 @@
+function xdot = objectEOM(t,x,rho,Cd,A,m,g,wind_vel)
+% Contributors: Ben Adams, 
+% Course number: ASEN 3801
+% File name: objectEOM.m
+% Created: 1/13/2026
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Inputs: velocity_body = column vector of aircraft air-relative velocity 
+%                         in body coordinates
+%                       = [u,v,w]’
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Outputs: wind_angles = [speed beta alpha]’
+%          speed = aircraft airspeed
+%          beta = side slip angle
+%          alpha = angle of attack
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Methodology: Use definitions to calculate wind angles and speed from 
+%               velocity vector
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%% 
+xdot = 0;
+end
+
