ecdc085a99b30c302388519e8cf9a09df04a455e
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-01-22 16:54:07 -0700

    bkjiui;gb

--- Lab1_prob2.m
+++ Lab1_prob2.m
@@ -35,9 +35,10 @@ vEE_0 = [0; 20; -20]; % starting velocity x position UPDATE!!
 % Statevector starting position
 x0 = [pEE_0; vEE_0];
 
-%% ODE options
+% ODE options
 options = odeset('RelTol',1e-8,'AbsTol',1e-8);
 
+%% part c
 %input options into brackets
 [tout,xout] = ode45(@(t,x) objectEOM(t,x,rho,Cd,A,m,g,wind_vel),tspan,x0, options);
 
@@ -55,6 +56,9 @@ xlabel('North (m)');
 ylabel('East (m)');
 zlabel('Height (m)');
 title('Problem 2c: 3D Trajectory of Object');
+zlim([0 40])
+ylim([0 100])
+view(90,0)
 
 hold off
 
@@ -70,4 +74,48 @@ for i = 1:length(var_wind_speeds)
 
 
 end
+
+%% Problem 2 part f
+% Varriable Mass | Const. Wind Velocity | Const. Rho | Const. Specific Energy
+figure(Name="Problem 2 - part f")
+hold on
+
+% varying Mass
+var_m = [0.025 .05 .075 0.1 0.125]; %[kg]
+vEE_0_hat = vEE_0/norm(vEE_0);
+KE_original = m*vEE_0;
+
+for i = 1:5
+    % new velocity vectors
+    %KE = .5*m*v
+    var_vEE_0 = 2*KE_original*(1/var_m(i));
+    var_x0 = [pEE_0; vEE_0];
+
+    % output x
+    [tout,xout] = ode45(@(t,x) objectEOM(t,x,rho,Cd,A,var_m(i),g,wind_vel),tspan,var_x0, options);
+    
+    xE_E = xout(:,1);
+    yE_E = xout(:,2);
+    zE_E = xout(:,3);
+
+    
+    %-zE_E to flip the Down axis into height
+    plot3(xE_E, yE_E,-zE_E,'LineWidth',2);
+end
+
+grid on;
+xlabel('North (m)');
+ylabel('East (m)');
+zlabel('Height (m)');
+title('Problem 2f: 3D Trajectory of Object');
+legend('0.025 kg','0.050 kg','0.075 kg', '0.100 kg' ,'0.125 kg')
+zlim([0 25])
+ylim([0 80])
+view(90,0)
+hold off
+
+
+
+
+
 
